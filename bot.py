34df7ed54cf9d15a5d33074257cb905fcdcbd5f3
sitkeni <125894692+sitkeni@users.noreply.github.com>
2023-02-24 21:20:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from telebot import TeleBot
 
 TOKEN = "6235950931:AAEqaoZ5MVzXuVy-HKZIFmrAsWPzzpBT0HU"
 bot = TeleBot(token=TOKEN)
-bot.message_handler(commands=['start', 'help'])
+bot.message_handler(commands=['start'])
 async def start(message):
-    await msg.reply_to_message(f'Я бот .привет')
+    await msg.reply_to_message(f'Я бот .Привет')
 bot.message_handler(content_types=['text'])
 async def start2(message):
-    if msg.text.lower() == 'привет':
+    if msg.text.lower() == 'Привет':
         await msg.answer('Привет!')
     else:
         await msg.answer('Не понимаю')
