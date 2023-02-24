b63d708a83fee7a27f4c74f853222139ca07f8ca
sitkeni <sitkeni@yandex.ru>
2023-02-24 19:31:53 +0300

    Первая версия бота

---
+++ bot.py
@@ -0,0 +1,18 @@
+from aiogram import Bot
+from aiogram import types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+TOKEN = "6235950931:AAEqaoZ5MVzXuVy-HKZIFmrAsWPzzpBT0HU"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+@dp.message_handler(commands=['star', 'help'])
+async def send_welcom(msg: types.message):
+    await msg.reply_to_message(f'Я бот .привет, {msg.from_user.first_name}')
+@dp.message_handler(content_types=['text'])
+async def get_text_messages(msg: types.message):
+    if msg.text.lower() == 'привет':
+        await msg.answer('Привет!')
+    else:
+        await msg.answer('Не понимаю')
+if __name__ == '__name__':
+    executor.start_polling(dp)
