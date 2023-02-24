62e9048475f842e4397f09d3b6350df2f2138369
sitkeni <125894692+sitkeni@users.noreply.github.com>
2023-02-24 19:35:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.utils import executor
 TOKEN = "6235950931:AAEqaoZ5MVzXuVy-HKZIFmrAsWPzzpBT0HU"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-@dp.message_handler(commands=['star', 'help'])
+@dp.message_handler(commands=['/start', '/help'])
 async def send_welcom(msg: types.message):
     await msg.reply_to_message(f'Я бот .привет, {msg.from_user.first_name}')
 @dp.message_handler(content_types=['text'])
